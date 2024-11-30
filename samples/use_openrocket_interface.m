5b8ab141183749ebf15b455b993d07266c8c6ad7
Yevgeniy Gorbachev <yevgeniy.gorbachev@gmail.com>
2024-11-30 09:21:48 -0500

    Expanded OpenRocket sample to show parameter sweeps in rocket and setup

--- samples/use_openrocket_interface.m
+++ samples/use_openrocket_interface.m
@@ -1,11 +1,58 @@
-omen = openrocket("data/OMEN.ork");
-sim = omen.sims(1);
-data = openrocket.get_data(sim, ["Altitude", "Vertical velocity", "Total velocity"]);
+clear; close all;
 
-figure;
+%% Basic plots
+otis = openrocket("data/OTIS.ork");
+sim = otis.sims(1);
+data = openrocket.get_data(sim);
+
+figure(name = "Basic plots");
 tiledlayout(2,1);
+
 nexttile;
-plot(data.Time, data.Altitude);
+plot_openrocket(data, "Altitude", "Total velocity", ...
+    end_ev = "GROUND_HIT", labels = ["BURNOUT", "APOGEE", "MAIN"]);
+
 nexttile;
-plot(data.Time, data.("Vertical velocity"));
-plot(data.Time, data.("Total velocity"));
+plot_openrocket(data, "Stability margin", "Angle of attack", ...
+    start_ev = "LAUNCHROD", end_ev = "APOGEE", labels = ["LAUNCHROD", "BURNOUT"]);
+
+%% Fin height sweep
+otis = openrocket("data/OTIS.ork");
+sim = otis.sims(1);
+fins = otis.component("Trapezoidal Fins");
+fh = fins.getHeight();
+
+heights = fh * (0.8:0.05:1.2); % vary height from 80 to 120%
+ssms = zeros(1, length(heights));
+for i = 1:length(heights)
+    fins.setHeight(heights(i));
+    data = openrocket.get_data(sim, "Stability margin");
+    ssms(i) = data{eventfilter("BURNOUT"), "Stability margin"};
+end
+
+figure(name = "Fin height sweep");
+plot(100*heights, ssms);
+xlabel("Fin height [cm]");
+ylabel("Stability at BURNOUT [cal]");
+yregion([4 Inf], FaceColor = [0.8 0.1 0.1]);
+
+%% Wind speed sweep
+otis = openrocket("data/OTIS.ork");
+sim = otis.sims(1);
+opts = sim.getOptions();
+
+winds = 1:10;
+aoa = zeros(1, length(winds));
+for i = 1:length(winds)
+    opts.setWindSpeedAverage(winds(i));
+    data = openrocket.get_data(sim, "Angle of attack");
+    aoa(i) = data{eventfilter("LAUNCHROD"), "Angle of attack"};
+end
+
+figure(name = "Wind speed sweep");
+plot(winds, rad2deg(aoa));
+xlabel("Average wind speed [m/s]");
+ylabel("Angle of attack at LAUNCHROD [deg]");
+yregion([15 Inf], FaceColor = [0.8 0.1 0.1]);
+
+profile viewer;
