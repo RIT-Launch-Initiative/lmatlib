c681c254994533d525897863bd58f5f130fd7dc1
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2025-01-12 21:08:00 -0500

    xarray: reworked sample to use wind data

--- samples/use_xarray.m
+++ samples/use_xarray.m
@@ -1,15 +1,46 @@
 clear;
 
-xarr = xarray(randi(100, 10, 50, 3), ...
-    time = seconds(1:10), x = linspace(0, 10, 50), ...
-    name = ["first", "second", "third"])
-another_xarr = xarray(randi(100, 100, 50, 3), ...
-    time = seconds(11:110), x = linspace(0, 10, 50), ...
-    name = ["first", "second", "third"]) 
-xarr = cat("time", xarr, another_xarr)
-xarr = permute(xarr, ["name", "x", "time"])
-xarr = xarr.range(x = [2 5]) % select by membership in range
-xarr = xarr.pickt(time = [seconds(4), seconds(1)], x = [4 0.5]) % select by tolerance around value
-xarr = xarr.pick(name = "second") % select by 
-another_xarr = another_xarr.range(x = [2 5]).pickt(time = [seconds(20) seconds(2)]) % chain selections
+data_file = pfullfile("samples", "data", "gfs_wind.mat");
+load(data_file);
+disp(gfs_wind);
+pressures = extract(gfs_wind.layer, digitsPattern); % The layer is formatted as <pressure>-ISBL, extract this number
+pressures = str2double(pressures); % convert to double
+gfs_wind.layer = pressures; % reassign text "layer" axis to use pressure
+gfs_wind = sort(gfs_wind, "layer", "descend"); % sort by descending pressure (increasing height)
 
+% subset of data, lat/lon range of roughly the continental US
+conus = gfs_wind.range(lat = [25 50], lon = [-125 -70]); 
+mid_alt = conus.pickt(layer = [80e3 10e3]); % pick data 80 +/- 10 kPa 
+high_alt = conus.range(layer = [-Inf 70e3]); % pick data up to 70 kPa
+mid_and_high_alt = cat("layer", mid_alt, high_alt); % glue data back together using dimension name
+
+roc = mid_alt.pick(lat = 43, lon = -77.5);
+h = roc.pick(element = "HGT");
+u = roc.pick(element = "UGRD");
+v = roc.pick(element = "VGRD");
+mag = (u^2 + v^2)^0.5;
+
+T_kelvin = roc.pick(element = "TMP") + 273.15;
+rho = (1/287.05) * T_kelvin.layer / squeeze(T_kelvin); % re-arranged specific form of ideal gas law
+
+figure;
+hold on;
+plot(mid_alt.layer, mid_alt.pick(element = "HGT", lat = 43, lon = -77)); % plot height against pressure level
+plot(mid_and_high_alt.layer, mid_and_high_alt.pick(element = "HGT", lat = 43, lon = -77), ":");
+xlabel("Pressure [Pa]");
+ylabel("Height [gpm]");
+
+figure;
+hold on;
+plot(u, h, DisplayName = "eastward");
+plot(v, h, DisplayName = "northward");
+plot(mag, h, DisplayName = "total");
+xlabel("Wind speed [m/s]");
+ylabel("Height [gpm]");
+legend;
+
+figure;
+hold on;
+plot(rho, h);
+xlabel("Air density [kg/m^3]");
+ylabel("Height [gpm]");
