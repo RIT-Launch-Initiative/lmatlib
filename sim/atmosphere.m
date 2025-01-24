abe0d42f2e179a93aeefd587cd66e28fabb2709d
Yevgeniy Gorbachev <yevgeniy.gorbachev@gmail.com>
2025-01-23 21:37:19 -0500

    atmosphere: Applied xarray and nwpdata changes

--- sim/atmosphere.m
+++ sim/atmosphere.m
@@ -1,151 +1,104 @@
 % NOTE: Requires Mapping Toolbox, <nwpdata>, and <xarray>
-classdef atmosphere
+classdef atmosphere < handle
     properties (GetAccess = public, SetAccess = protected)
-        data % xarray;
-        raster % {mustBeA(raster, ["map.rasterref.MapPostingsReference", ...
-            % "map.rasterref.GeographicPostingsReference"])};
-        epoch (1,1) {mustBeA(epoch, ["datetime", "duration"])} = seconds(0);
+        ref nwpdata;
+        prs_data % xarray;
+        prs_sampler function_handle;
+        epoch (1,1) datetime = missing;
     end
 
     properties (Access = protected)
-        intrinsic_lat (1, 1) string;
-        intrinsic_lon (1, 1) string
-        sample3_template (:, :) %xarray;
-        interpolant (1, 1) griddedInterpolant;
+        method (1, 1) string;
+        extrap (1, 1) string;
+        istimed (1, 1) logical = false;
+    end
+
+    properties (Constant, Access = protected)
+        atmos_fields = ["HGT", "UGRD", "VGRD", "TMP"];
+        prs_regex = "(\d+)-ISBL";
     end
     
     methods
-        function obj = atmosphere(paths, params)
+        function obj = atmosphere(ref, params)
             arguments
-                paths (:, 1) {mustBeFile};
-                params.fields (1, :) string = ["HGT", "UGRD", "VGRD", "TMP"];
+                ref (1,1) nwpdata;
                 params.lats (1, 2) double = [-Inf Inf];
                 params.lons (1, 2) double = [-Inf Inf];
+                params.method (1, 1) string = "linear";
+                params.extrap (1, 1) string = "none";
             end
 
-            data_array = cell(1, length(paths));
-            isbl_regex = "(\d+)-ISBL";
-
-            for i = 1:length(paths)
-                [data, raster] = nwpdata.read(paths(i), ...
-                    fields = params.fields, layers = isbl_regex, ...
-                    lats = params.lats, lons = params.lons);
-                baro_levels = string(regexp(string(data.layer), isbl_regex, "tokens"));
-                data.layer = str2double(baro_levels);
-                data = data.rename("layer", "pressure");
-                data_array{i} = sort(data, "pressure", "descend");
+            if ~isdownloaded(ref)
+                error("NWP data not downloaded");
+            end
+            obj.ref = ref;
+            if isa(obj.ref.crs, "geocrs")
+                vertical = "lat";
+                horizontal = "lon";
+            elseif isa(obj.ref.crs, "projcrs")
+                vertical = "y";
+                horizontal = "x";
             end
 
-            obj.raster = raster;
-            raster_type = obj.raster.CoordinateSystemType;
-            % input case: planar or geographic
-            if raster_type == "planar"
-                obj.intrinsic_lat = "y";
-                obj.intrinsic_lon = "x";
-            elseif raster_type == "geographic"
-                obj.intrinsic_lat = "lat";
-                obj.intrinsic_lon = "lon";
-            end
+            % TODO restrict read data using raster metadata
+            prs_data = obj.ref.read(fields = atmosphere.atmos_fields, ...
+                layers = atmosphere.prs_regex, lats = params.lats, lons = params.lons);
+            keyboard
 
-            % input case: many time or 1 time
-            data = cat("time", data_array{:});
-            data = permute(data, [obj.intrinsic_lat, obj.intrinsic_lon, "time", "pressure", "field"]);
-            data = sort(data, [obj.intrinsic_lat, obj.intrinsic_lon, "time"], "ascend");
-            obj.epoch = data.time(1);
-            obj.data = data;
-            obj.sample3_template = data.index(...
-                obj.intrinsic_lat, 1, obj.intrinsic_lon, 1, "time", 1).squeeze;
+            % Convert text layers to numeric pressures
+            pressures = regexp(prs_data.layer, atmosphere.prs_regex, "tokens");
+            pressures = str2double(string(pressures));
+            prs_data.layer = pressures;
+            prs_data = prs_data.rename("layer", "pressure");
+            prs_data = sort(prs_data, "pressure", "descend");
 
-            if isscalar(data.time)
-                obj.interpolant = griddedInterpolant(data.coordinates(1:2), double(data), ...
-                    "linear", "nearest");
+            % Convert time
+            obj.epoch = prs_data.time(1);
+            prs_data.time = seconds(prs_data.time - obj.epoch);
+
+            obj.method = params.method;
+            obj.extrap = params.extrap;
+            if isscalar(prs_data.time)
+                obj.istimed = false;
+                axes = [vertical horizontal];
             else
-                coords = data.coordinates(1:3);
-                [~, ~, coords{3}] = obj.get_intrinsic(NaN, NaN, coords{3});
-                obj.interpolant = griddedInterpolant(coords, double(data), ...
-                    "linear", "nearest");
+                obj.istimed = true;
+                axes = [vertical horizontal "time"];
             end
+            obj.prs_data = prs_data;
+            obj.prs_sampler = prs_data.interpolant(axes, obj.method, obj.extrap);
         end
 
-        function [int_lat, int_lon, int_time] = get_intrinsic(obj, lat, lon, time)
-            % [int_lat, int_lon, int_time] = get_intrinsic(atmos, lat, lon, time)
-            % Convert geographic-datetime coordinates to intrinsic (interpolant) coordinates
-            % Time is converted to seconds since epoch
-            % Lat/lon are returned unmodified 
-            raster_type = obj.raster.CoordinateSystemType;
-            if raster_type == "planar"
-                [int_lon, int_lat] = projfwd(obj.raster.ProjectedCRS, lat, lon);
-            elseif raster_type == "geographic"
-                int_lat = lat;
-                int_lon = lon;
-            end
-            int_time = seconds(time - obj.epoch);
-        end
-
-        function result = sample3(obj, time, lat, lon)
+        function result = sample(obj, params)
             arguments
                 obj atmosphere;
-                time (1,1) {mustBeA(time, ["datetime", "duration"])};
-                lat (1,1) double;
-                lon (1,1) double;
-            end
-            if length(obj.interpolant.GridVectors) == 2
-                [int_lat, int_lon, ~] = obj.get_intrinsic(lat, lon, obj.epoch);
-                out = obj.interpolant(int_lat, int_lon);
-            elseif length(obj.interpolant.GridVectors) == 3
-                [int_lat, int_lon, int_time] = obj.get_intrinsic(lat, lon, time);
-                out = obj.interpolant(int_lat, int_lon, int_time);
+                params.lat (1,1) double {mustBeFinite};
+                params.lon (1,1) double {mustBeFinite};
+                params.time (1,1) datetime = NaT;
+                params.height (1,1) = NaN;
             end
 
-            result = obj.sample3_template;
-            result{:, :} = squeeze(out);
-        end
-
-        function varargout = sample4(obj, time, lat, lon, height, fields)
-            arguments
-                obj atmosphere;
-                time (1,1) {mustBeA(time, ["datetime", "duration"])};
-                lat (1,1) double;
-                lon (1,1) double;
-                height (1,1) double;
-                fields (1, :) string;
+            if isa(obj.ref.crs, "projcrs")
+                [x, y] = projfwd(obj.ref.crs, params.lat, params.lon);
+                sample = {y, x};
+            else
+                sample = {params.lat, params.lon};
             end
 
-            if nargout ~= length(fields)
-                warning("%d outputs requested, %d used", length(fields), nargout);
+            if obj.istimed
+                if ~isfinite(params.time)
+                    error("Sample time required for this atmosphere model")
+                end
+                sample{3} = seconds(params.time - obj.epoch);
             end
 
-            samp = sample3(obj, time, lat, lon);
-
-            interp_data = [samp.pressure, samp.double];
-            interp_h = samp.pick(field = "HGT").squeeze.double;
-            if height < min(interp_h)
-                height = min(interp_h);
+            result = obj.prs_sampler(sample).squeeze;
+            if isfinite(params.height)
+                data = [result.pressure, double(result)];
+                data = interp1(result.height, data, params.height);
+                result = xarray(data(2:end), ...
+                    fields = result.fields, pressure = data(1));
             end
-            result = interp1(interp_h, interp_data, height, "linear");
-            result = xarray(result, field = ["PRES"; samp.field]);
-
-            present = ismember(fields, result.field);
-            if any(~present)
-                error("Data fields %s not found", fields(present));
-            end
-            result = result.pick(field = fields).double;
-
-            varargout = cell(1, length(fields));
-            for i = 1:length(fields)
-                varargout{i} = result(i);
-            end
-        end
-    end
-
-    methods (Static)
-        function info = baroproducts
-            info.gfs_100 = "pgrb2.1p00";
-            info.gfs_p50 = "pgrb2.0p50";
-            info.gfs_p25 = "pgrb2.0p25";
-            info.nam_12km = "awphys";
-            info.nam_3km = "conusnest.hiresf";
-            info.hrrr_3km = "wrfprsf";
         end
     end
 end
