603c6ad35698d201d05787098714e45a7b326a02
Yevgeniy Gorbachev <yevgeniy.gorbachev@gmail.com>
2025-03-31 11:16:34 -0400

    moved elevationquery endpoint to more reliable OpenMeteo

--- data/elevationquery.m
+++ data/elevationquery.m
@@ -1,34 +1,19 @@
-function alt = elevationquery(lat, lon, opts)
-    % Read altitude data from the USGS Elevation Point Query Service
-    % alt = elevationquery(lat, lon[, Name, Value])
+function alt = elevationquery(lat, lon)
+    % Read altitude data from the OpenMeteo API
+    % alt = elevationquery(lat, lon)
     %   Inputs
     %   lat     (double)    latitude in degrees
     %   lon     (double)    longitude in degrees
-    %   Name-Value arguments
-    %   units   ({"m"} | "ft")  Output units
     %   Outputs
     %   alt     (double)    altitude
-    % See documents for <a href="https://www.usgs.gov/faqs/how-accurate-are-elevations-generated-elevation-point-query-service-national-map">accuracy</a>
     arguments
         lat (1,1) double;
         lon (1,1) double;
-        opts.units (1,1) string {mustBeMember(opts.units, ["ft", "m"])} = "m";
     end
-
-    if opts.units == "m"
-        unitstr = "Meters";
-    elseif opts.units == "ft"
-        unitstr = "Feet";
-    end
-
-    wopts = weboptions(ContentType = "json", Timeout = 10);
-    basepath = "https://epqs.nationalmap.gov/v1/json";
-    keys = sprintf("?x=%f&y=%f&units=%s&wkid=4326&includeDate=False", lon, lat, unitstr);
-    ret = webread(basepath + keys, wopts);
-    alt = ret.value;
-    % Feet return numeric result, Meters return char result for some reason
-    if ischar(alt) || isstring(alt)
-        alt = str2double(alt);
-    end
+    
+    wopts = weboptions(ContentType = "json");
+    url = sprintf("https://api.open-meteo.com/v1/elevation?latitude=%s&longitude=%s", ...
+        compose("%f", lat).join(","), compose("%f", lon).join(","));
+    ret = webread(url, wopts);
+    alt = ret.elevation;
 end
-
