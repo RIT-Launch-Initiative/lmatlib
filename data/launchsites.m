273b758ccf8b7aab0b7128a925dfc88bdac2cbec
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2025-04-28 22:51:17 -0400

    Updated exact Spaceport launch site

--- data/launchsites.m
+++ data/launchsites.m
@@ -1,6 +1,7 @@
 function info = launchsites(launchsite)
     siteinfo = dictionary;
-    siteinfo("spaceport-america") = struct(lat = 32.94030518807581, lon = -106.92182007667907, alt = 1381);
+
+    siteinfo("spaceport-america") = struct(lat = 32.94012563817728, lon = -106.91165732518579, alt = 1381);
     siteinfo("spaceport-midland") = struct(lat = 31.9485582, lon = -102.2086869, alt = 875);
     siteinfo("cedar-springs") = struct(lat = 37.7481336, lon = -113.2718213, alt = 1667);
     siteinfo("urrg") = struct(lat = 42.702986317065545, lon = -77.19120332086317, alt = 271);
