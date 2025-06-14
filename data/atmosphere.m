f07188770237bc07c8bafcde957e6b4df18230d2
Yevgeniy Gorbachev <yevgeniy.gorbachev@gmail.com>
2025-06-13 19:19:21 -0500

    Fixed atmosphere reading bug

--- data/atmosphere.m
+++ data/atmosphere.m
@@ -73,8 +73,8 @@ function atmos = atmosphere(model, product, lat, lon, time, opts)
     % if the specified result is on-the-hour, size(data, "time") = 1 and the interpolation fails
     if size(data, "time") > 1
         % convert time axis to numeric (seconds since first time) so interp() works
-        time.TimeZone = epoch.TimeZone;
         epoch = data.time(1);
+        time.TimeZone = epoch.TimeZone;
         data.time = seconds(data.time - epoch);
         data = interp(data, time = seconds(time - epoch));
     end
