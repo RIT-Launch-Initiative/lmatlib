ba828cb58fe61ecef70a9d12121d158aa05d2ab8
Yevgeniy Gorbachev <yevgeniy.gorbachev@gmail.com>
2025-06-22 14:36:58 -0500

    Added docline

--- plot/colorbartext.m
+++ plot/colorbartext.m
@@ -1,4 +1,6 @@
 function [cb] = colorbartext(label, varargin)
+    % Create colorbar with label
+    % cb = colorbartext(label, ...)
     cb = colorbar(varargin{:});
     cb.Label.String = label;
 end
